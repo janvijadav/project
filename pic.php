38dde71461402ed3fadb5fabdbd09936c38931e2
janvijadav <110669936+janvijadav@users.noreply.github.com>
2022-10-18 17:07:34 +0530

    Update pic.php

--- pic.php
+++ pic.php
@@ -1,32 +1,36 @@
+//w.a.p to upload,rename and display pic. in table.
+
 <?php
    $con=mysqli_connect("localhost","root","","janvi");
-   if(isset($_FILES['picture']))
+   if(isset($_FILES['pic']))
 	{   
         
-	    $filename=$_FILES['picture'];
-		$target_dir="image/";
-		$target_file=$target_dir.basename($_FILES["picture"]["name"]);
+	    $filename=$_FILES['pic'];
+		$target_dir="pic/";
+		$target_file=$target_dir.basename($_FILES["pic"]["name"]);
 		$temp=explode(".",$_FILES['pic']['name']);
 		$newfilename=rand(35000,350000).'.'.end($temp);
 		move_uploaded_file($_FILES["pic"]["tmp_name"],$target_dir.$newfilename);
-		$sql="INSERT INTO `image`(`pic`) VALUES ('$newfilename')";
+		$sql="INSERT INTO `pic`(`pic`) VALUES ('$newfilename')";
 		$res=mysqli_query($con,$sql);
    }
 ?>
 
-<head>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/js/bootstrap.min.js"></script>
+ 
+ <head>
+	   
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.1/css/bootstrap.min.css"/>
 </head>
+<div class="container-fluid mt-2">
  <form action="pic.php" method="POST" enctype="multipart/form-data">
-<input type="file" name="pic" accept=".jpg ,.png,.jpeg">
-<input type ="submit" value="save">
-
+<input type="file"  class="from-control" name="pic"  accept=".jpg,.png,.jpeg"></br></br>
+<input type ="submit" value="save" class="btn btn-primary">
+</div>
 </form>
 <table class="table table-bordered text-center ">
 <div class="container mt-2">
 <tr>
-	<th>picture
+	<th>pic
   <?php
 		$sql="SELECT * FROM `pic`";
 		$res=mysqli_query($con,$sql);
@@ -34,9 +38,9 @@
 		{
  ?>
   <tr>
-     <td><img src="<?php echo "image/".$row['picture'];?>" height="100" width="100">
+     <td><img src="<?php echo "pic/".$row['pic'];?>" height="100" width="100">
 	<?php
 	      }
 		?>
 </div>
-</table>		 
+</table>		 		 
